0e3210e61de72444ecf6d88d053a470954c1e75f
Simón Menendez Bravo <menendezsimon@gmail.com>
2025-03-23 18:13:56 +0100

    Sixth commit: authentication working

--- app/Http/Controllers/ProgramController.php
+++ app/Http/Controllers/ProgramController.php
@@ -12,7 +12,6 @@ class ProgramController extends Controller
     public function show(Program $program)
     {
         $sessions = UserSession::with(['trainingSessions.exercise'])->where('program_id', $program->id)->get();
-        
         return view('program.show', compact('program', 'sessions'));
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,29 @@
 <?php
+
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProgramController;
 use App\Http\Controllers\SessionController;
 
 Route::get('/', function () {
-   return view('welcome');
+    return view('welcome');
 });
-
-Route::resource('users', UserController::class);
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+Route::post('/users', [UserController::class, 'store'])->name('users.store');
 Route::get('/program/{program}', [ProgramController::class, 'show'])->name('program.show');
 Route::get('/session/{session}', [SessionController::class, 'show'])->name('session.show');
-?>
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
